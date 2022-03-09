eea73814c240865c8a4f099745a6c9fb2b7f9b43
turbits <turbits@protonmail.com>
2022-03-09 12:12:41 -0500

    fix/refac db connect

--- server/api/lib/connectToDatabase.mjs
+++ server/api/lib/connectToDatabase.mjs
@@ -1,15 +1,17 @@
+import "dotenv/config";
+
 import mongoose from "mongoose";
 
 // SETUP
-const { NODE_ENV, DB_LOCAL_URI, DB_PROD_URI, DB_NAME } = process.env;
-const PORT = process.env.PORT || 4000;
+const { NODE_ENV, DB_LOCAL_URI, DB_PROD_URI } = process.env;
+const PORT = process.env.PORT || 27017;
 let databaseUrl = "";
 let cached = global.mongoose;
 
 if (NODE_ENV === "production") {
   databaseUrl = `${DB_PROD_URI}?retryWrites=true&w=majority`;
 } else if (NODE_ENV === "development") {
-  databaseUrl = `${DB_LOCAL_URI}:${PORT}/${DB_NAME}`;
+  databaseUrl = DB_LOCAL_URI;
 } else {
   throw new Error("NODE_ENV not set");
 }
@@ -18,9 +20,9 @@ if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-export const connectToDatabase = async () => {
+export const ConnectToDatabase = async () => {
   if (cached.conn) {
-    console.log(`🟦 cached connection found: ${cached.conn}`);
+    console.log(`🟦 cached connection found`);
     return cached.conn;
   }
 
